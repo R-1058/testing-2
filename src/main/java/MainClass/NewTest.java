cb84fd6ed46df13c2f4d9257c599d806eaff6cc8
ARAVINDHAN G <A1354@SVSGLOBAL.COM>
2022-07-11 11:57:08 +0530

    new path

---
+++ src/main/java/MainClass/NewTest.java
@@ -0,0 +1,30 @@
+package MainClass;
+
+import BC.BaseClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+public class NewTest  {
+    public WebDriver driver;
+    @Test
+    public void openMyBlog() {
+        driver.get("https://www.softwaretestingmaterial.com/");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\a1354\\eclipse-workspace\\Shriam_General_Insurance\\Git_Integration\\src\\main\\java\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
